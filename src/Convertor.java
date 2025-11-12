e6a0b75d1a80b8aa26217aec8582070d2cc57de9
MonacoBz <gerardossj2@hotmail.com>
2025-11-11 20:19:32 -0600

    Clases bases listas

---
+++ src/Convertor.java
@@ -0,0 +1,16 @@
+public class Convertor {
+
+    private Integer monto;
+
+    public Convertor(Integer monto) {
+        this.monto = monto;
+    }
+
+    public Integer getMonto() {
+        return monto;
+    }
+
+    public void setMonto(Integer monto) {
+        this.monto = monto;
+    }
+}
